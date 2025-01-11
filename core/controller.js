baa18dee73b80222b0029a0f21cc11d3366cf5e0
ADH <ahartens@gmail.com>
2025-01-11 08:02:57 +0100

    added docstring

--- core/controller.js
+++ core/controller.js
@@ -1,6 +1,12 @@
 
+
 export class Controller {
+  /*
+    This is the base class for all controllers.
+    It is used to initialize the components for the controller ie model and view.
+    */
   constructor() {
+
     this._initializeComponents();
   }
 
