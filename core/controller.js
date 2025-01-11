ac1bb2be2a458ecb592a20ee98596ed7d31e5e0f
ADH <ahartens@gmail.com>
2025-01-11 08:07:54 +0100

    test

--- core/controller.js
+++ core/controller.js
@@ -4,6 +4,9 @@ export class Controller {
   /*
     This is the base class for all controllers.
     It is used to initialize the components for the controller ie model and view.
+
+
+    TEST
     */
   constructor() {
 
